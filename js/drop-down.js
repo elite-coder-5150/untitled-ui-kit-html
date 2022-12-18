873bb4aa3ce0daf6241c9b61b2c6c243250305cd
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-18 14:06:02 -0600

    figuring out the bug in the javascript with toggling a class:(

--- js/drop-down.js
+++ js/drop-down.js
@@ -1,17 +1,26 @@
 const accountBtn = document.querySelector('.md-secondary')
-const profileCard = document.querySelector('.profile-card');
-
-
-const toggleClass = (cls) => {
-    if (cls) {
-        profileCard.classList.add("show")
+const dropDown = document.querySelector('.drop-down');
+const profileCard = document.querySelector('.profile-card')
+const toggleClass = () => {
+    if (profileCard.classList.contains('show')) {
+        profileCard.classList.remove("show")
+    } else {
+        profileCard.classList.add("show");
     }
 }
 
 toggleClass();
 
+const addClass = (cls) => {
+    if (cls) {
+        this.classList.add(cls)
+    } else {
+        this.classList.remove(cls);
+    }
+}
+profileCard.addClass('show');
 accountBtn.addEventListener('click', () => {
-    profileCard.style.display = 'flex'
+    dropDown.addClass('show');
     
 });
 
